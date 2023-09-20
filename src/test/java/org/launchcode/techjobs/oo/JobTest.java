89af7fe4d04e88954f74777e9b43682dbba85b57
Erin.Schneider <erinsuzy@gmail.com>
2023-09-19 19:34:32 -0500

    jobtest1

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,25 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import javax.naming.Name;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
-    //TODO: Create your unit tests here
+
+
+    @Test
+    public void testSettingJobId() {
+      Job newJob = new Job();
+      Job alsoNewJob = new Job();
+      assertNotEquals(newJob.getId(), alsoNewJob.getId(), .001);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+       Job tester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(tester.getName() != null);
+        assertEquals(tester.getName(), "Product tester");
+    }
 }
